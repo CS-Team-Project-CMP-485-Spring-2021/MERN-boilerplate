123806813f328e171e032ed0d087a77d77f18c9b
Rene C <lilymist2@gmail.com>
2021-05-11 21:30:00 -0400

    finished Patient files

--- client/app/components/Patient/Patient.js
+++ client/app/components/Patient/Patient.js
@@ -1,15 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Table from './Table';
+import Appointment from './Appointment';
 
 
+class Patient extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            appointments: []
+        };  
+    }
 
+    
 
+    handleSubmit = appt => {
+        this.setState((state) => ({
+            appointments: state.appointments.concat(appt)
+        }));
+    }
+    
 
+    render() {
 
-// const Patient = () => (
-//   <div>
-//     <Patient/> 
-//   </div>
-// ); 
+        const appoint = this.state.appointments;
 
+        return (
+            <div className="container">
+                <h1 style={{color:'black', textDecoration: 'underline black'}}>Your Appointments</h1>
+            
+                <Table apptData = {appoint}/>
+                
+                <br/>
+                    <h3 style={{color:'black', textDecoration: 'underline black' }}>Set Up An Appointment</h3>
+                    <Form handleSubmit = {this.handleSubmit}/>
+                <br/>
+
+            </div>
+        );
+    }
+}
 
 export default Patient;
---
+++ client/app/components/Patient/Table.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+const TableHeader = () => { 
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Date/Time</th>
+                <th>First Visit?</th>
+                <th>Department</th>
+                <th>Doctor</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => { 
+    const rows = props.apptData.map((row, index) => {
+    
+        return (
+            <tr key={index}>
+                <td>{row.Name}</td>
+                <td>{row.date}</td>
+                <td>{row.option}</td>
+                <td>{row.Department}</td>
+                <td>{row.Doctor}</td>
+            </tr>
+        );
+    });
+
+    return <tbody>{rows}</tbody>;
+}
+
+const Table = (props) => {
+   const apptData = props;
+
+    return (
+        <div>
+            <table>
+                <TableHeader />
+                <TableBody apptData = {apptData}/>
+            </table>
+        </div>
+
+    )
+
+}
+
+export default Table;
