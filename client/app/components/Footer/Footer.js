e822302457b0ad9f922b9df10c685121273aeeaa
Tes <43484797+CallMeTes@users.noreply.github.com>
2021-05-16 17:56:42 -0400

    Edited Footer

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <hr />
-    <p>Footer</p>
+    <p>Medical Blockchain Presented by Team 2 - Rene, Lisangel, Patience, Elija, George, Tesfa, Alvin, Nobi, Lenin</p>
   </footer>
 );
 
